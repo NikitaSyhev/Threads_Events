173c842b3dd5a8cc7e3348275c5c615e2ecf6f4c
ADMIN <ADMIN@DESKTOP-CBRV7QC>
2023-12-27 21:03:17 +0500

    done

--- Threads_repetition/Form1.Designer.cs
+++ Threads_repetition/Form1.Designer.cs
@@ -28,39 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.numMax = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
             this.chart1.Location = new System.Drawing.Point(0, 0);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
             this.chart1.Size = new System.Drawing.Size(788, 448);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Пауза";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numMin
+            // 
+            this.numMin.Location = new System.Drawing.Point(668, 123);
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(120, 20);
+            this.numMin.TabIndex = 2;
+            this.numMin.ValueChanged += new System.EventHandler(this.numMax_ValueChanged);
+            // 
+            // numMax
+            // 
+            this.numMax.Location = new System.Drawing.Point(668, 178);
+            this.numMax.Name = "numMax";
+            this.numMax.Size = new System.Drawing.Size(120, 20);
+            this.numMax.TabIndex = 3;
+            this.numMax.ValueChanged += new System.EventHandler(this.numMax_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numMax);
+            this.Controls.Add(this.numMin);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +105,9 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.NumericUpDown numMax;
     }
 }
 
--- Threads_repetition/Form1.cs
+++ Threads_repetition/Form1.cs
@@ -31,8 +31,10 @@ namespace Threads_repetition
         public event MethoContainer onExit;
         //создали поток
         public Thread thread;
+        
         //начальная координата
         public int x = 0;
+        public CancellationTokenSource cts = new CancellationTokenSource();
 
 
         // метод для создания потока и запускается при открытии формы
@@ -47,7 +49,7 @@ namespace Threads_repetition
         {
             Thread.Sleep(1000);
             Random rnd = new Random();
-            int randomNum = rnd.Next(0, 100);
+            int randomNum = rnd.Next((int)numMin.Value, (int)numMax.Value);
             //метод Invoke принимает делегат и выполняет его в потоке
             Invoke((MethodInvoker)delegate
             {
@@ -56,6 +58,46 @@ namespace Threads_repetition
             );
             x++;
             onExit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(thread.ThreadState == ThreadState.Stopped) {
+                button1.Text = "Pause";
+                thread.Resume();
+            }
+            else
+            {
+                button1.Text = "Resume";
+                thread.Suspend();
+            }
+        }
+
+      
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            if (thread.ThreadState == ThreadState.Suspended) thread.Resume();
+            thread.Abort();
+            cts.Cancel();
+            
+        }
+
+        private void numMax_ValueChanged(object sender, EventArgs e)
+        {
+            //thread.Suspend();
+            if(numMax.Value < numMin.Value)
+            {
+                if((sender as NumericUpDown).Name == "numMax") {
+                    numMax.Value = numMin.Value;
+                }
+                else
+                {
+                    numMin.Value = numMax.Value;
+                }
+            }
+            //if(!(thread.ThreadState == ThreadState.Running))  thread.Resume();
+
         }
     }
 }
